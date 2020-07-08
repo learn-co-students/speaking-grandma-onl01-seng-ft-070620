753ab0cefbee79fa259482b271f9c34b1735ccc8
Jorge Venegas Martinez <brangelina01@gmail.com>
2020-07-08 04:15:39 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,24 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma (phrase)
+  if phrase.upcase != phrase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else 
+    "NO, NOT SINCE 1938!"
+  end 
+  
+end   
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
